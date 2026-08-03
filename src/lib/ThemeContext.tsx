e13afd83ad512f294442a1885bd60f62c76f9e45
ankit kumar <ankitkrth1911@gmail.com>
2026-08-03 19:05:05 +0530

    Update ThemeContext.tsx

--- src/lib/ThemeContext.tsx
+++ src/lib/ThemeContext.tsx
@@ -18,7 +18,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
         return storedTheme;
       }
 
-      return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+      return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" ;
     }
   );
 
